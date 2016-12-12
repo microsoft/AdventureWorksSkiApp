966ac6f6a6c0c141bdcb48017bf190bff59d1239
Carlos Fernández Llamas <sirikon2@gmail.com>
2016-12-12 21:45:13 +0100

    Working on Weather Widget View
    Working on Android, missing config and iOS resources

--- src/SkiResort.XamarinApp/SkiResort.XamarinApp/Views/WeatherWidgetItem.xaml.cs
+++ src/SkiResort.XamarinApp/SkiResort.XamarinApp/Views/WeatherWidgetItem.xaml.cs
@@ -11,15 +11,25 @@ namespace SkiResort.XamarinApp.Views
     public partial class WeatherWidgetItem : ContentView
     {
         public static readonly BindableProperty IconProperty = BindableProperty.Create("Icon", typeof(string), typeof(WeatherWidgetItem), null);
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(WeatherWidgetItem), null);
+        public static readonly BindableProperty ValueProperty = BindableProperty.Create("Value", typeof(string), typeof(WeatherWidgetItem), null);
         public string Icon {
             get { return (string)GetValue(IconProperty); }
             set { SetValue(IconProperty, value); }
         }
-
-        public string Title { get; set; }
-        public string Value { get; set; }
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
         public WeatherWidgetItem()
         {
+            BindingContext = this;
             InitializeComponent();
         }
     }
