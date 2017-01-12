ea5de4a3cc76d71c0896629dfc475c575ec441f5
Carlos Fernández Llamas <sirikon2@gmail.com>
2017-01-12 09:09:00 +0100

    Working on Dining, also created some Solution Folders

---
+++ src/SkiResort.XamarinApp/SkiResort.XamarinApp/Services/RestaurantsService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using SkiResort.XamarinApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiResort.XamarinApp.Services
+{
+    class RestaurantsService
+    {
+        public async Task<List<Restaurant>> GetRestaurants()
+        {
+            var httpService = new HTTPService("http://adventureworkskiresort.azurewebsites.net/api");
+            var restaurantsData = await httpService.Get("/restaurants/nearby?latitude=0&longitude=0");
+            var restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(restaurantsData);
+            return restaurants;
+        }
+    }
+}
---
+++ src/SkiResort.XamarinApp/SkiResort.XamarinApp/Views/DiningView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace SkiResort.XamarinApp.Views
+{
+    public partial class DiningView : ContentView
+    {
+        public DiningView()
+        {
+            InitializeComponent();
+        }
+    }
+}
