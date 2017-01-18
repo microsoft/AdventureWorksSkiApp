23ff6697319bc1b7aa54c827ebdc924e063a63c7
Carlos Fernández Llamas <sirikon2@gmail.com>
2017-01-18 10:43:40 +0100

    Lift Detail map marker

---
+++ src/SkiResort.XamarinApp/SkiResort.XamarinApp/Converters/LiftToAltitudeFormattedText.cs
@@ -0,0 +1,28 @@
+﻿using SkiResort.XamarinApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SkiResort.XamarinApp.Converters
+{
+    class LiftToAltitudeFormattedText : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var lift = value as Lift;
+            var formattedText = new FormattedString();
+            formattedText.Spans.Add(new Span { Text = lift.Name, FontSize = 13 });
+            formattedText.Spans.Add(new Span { Text = " 300ft", FontAttributes = FontAttributes.Bold, FontSize = 13 });
+            return formattedText;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
